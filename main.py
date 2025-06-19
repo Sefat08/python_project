83f2949f1318db22126cf59832ebd0214ab8ef00
Sefat Moyeen <sefat08@Sefats-MacBook.local>
2025-06-19 15:16:47 +0200

    cleaned data

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import pandas as pd
 # Get the current working directory of the python file that is running
 CURRENT_DIRECTORY = os.getcwd()
 
-
 # We know that the data is actually in a 'data' folder. So, we create a path with the current directory and add the data string
 DATA_DIRECTORY = os.path.join(CURRENT_DIRECTORY, 'data')
 
 # We then find out the path of all the files in that folder
 FILE_PATH_LIST = os.listdir(DATA_DIRECTORY)
 
-print(FILE_PATH_LIST)
-
 IMPORTED_FILES = []
 
-for path in FILE_PATH_LIST:
-    csv_file = pd.read_csv(os.path.join(DATA_DIRECTORY, path), encoding='latin-1')
-    IMPORTED_FILES.append(csv_file)
+for file in FILE_PATH_LIST:
+    # Get the full path of the file
+    # Import using pandas
+    # Add to a list
+    pass
+
+# Create a datasturucte from which we can search easily
+
+# list_of_stations = [station1, station2, station3]
+
+# station = list_of_stations.find(input)
+# station.name
+# station.average
+
+# station = {
+#     'name': 'John doe',
+#     'age': 12,
+#     'subjects': ['Math', 'Physics']
+#}
