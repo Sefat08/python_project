1833647d938b66b2c90b65c61cbed1ed5ca06b16
Sefat <sefat.moyeen@gmail.com>
2025-06-19 18:32:41 +0200

    Added functions to get matching results

--- main.py
+++ main.py
@@ -7,27 +7,47 @@ CURRENT_DIRECTORY = os.getcwd()
 # We know that the data is actually in a 'data' folder. So, we create a path with the current directory and add the data string
 DATA_DIRECTORY = os.path.join(CURRENT_DIRECTORY, 'data')
 
-# We then find out the path of all the files in that folder
-FILE_PATH_LIST = os.listdir(DATA_DIRECTORY)
+station_name = input('Enter the name of the station: ').strip().lower()
 
-IMPORTED_FILES = []
+station_list = pd.read_csv(os.path.join(DATA_DIRECTORY, 'station_list.csv'), encoding='latin-1')
+temperature_data = pd.read_csv(os.path.join(DATA_DIRECTORY, 'temperature_data.csv'), encoding='latin-1')
+precipitation_data = pd.read_csv(os.path.join(DATA_DIRECTORY, 'precipitation_data.csv'), encoding='latin-1')
 
-for file in FILE_PATH_LIST:
-    # Get the full path of the file
-    # Import using pandas
-    # Add to a list
-    pass
 
-# Create a datasturucte from which we can search easily
+def get_matching_stations(station_list, station_name):
+    matching_stations = []
+    for index, row in station_list.iterrows():
+        station = row['Stationsname'].strip().lower()
+        if station_name in station:
+            matching_stations.append(row)
 
-# list_of_stations = [station1, station2, station3]
+    return matching_stations
 
-# station = list_of_stations.find(input)
-# station.name
-# station.average
 
-# station = {
-#     'name': 'John doe',
-#     'age': 12,
-#     'subjects': ['Math', 'Physics']
-#}
+def get_temperature(matching_stations):
+    matching_rows = []
+    for row in matching_stations:
+        station_id = row['Stations_id']
+        
+        for _, t_row in temperature_data.iterrows():
+            if t_row['Stations_id'] == station_id:
+                matching_rows.append(t_row)
+    return matching_rows
+
+def get_precipitation(matching_stations):
+    matching_rows = []
+    for row in matching_stations:
+        station_id = row['Stations_id']
+        
+        for _, p_row in precipitation_data.iterrows():
+            if p_row['Stations_id'] == station_id:
+                matching_rows.append(p_row)
+    return matching_rows
+        
+matching_stations = get_matching_stations(station_list, station_name)
+
+matching_temperatures = get_temperature(matching_stations)
+matching_precipitaitons = get_precipitation(matching_stations)
+
+print(matching_stations)
+print(matching_temperatures)
