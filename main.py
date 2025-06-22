5a1e936afc4e83a3ea1a5ebae9a4e1066eb3ed2e
form1dable <syef.dev@gmail.com>
2025-06-22 11:52:43 +0200

    manual calculation of averages

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
+import pprint
+from numpy import average
 import pandas as pd
+import difflib
+
+from pandas._libs.lib import count_level_2d
 
 # Get the current working directory of the python file that is running
 CURRENT_DIRECTORY = os.getcwd()
@@ -22,22 +27,23 @@ MONTHS_LIST = [
     "Dez",
 ]
 
-station_name = input("Enter the name of the station: ").strip().lower()
+
+print("Loading dataset")
 
 station_list = pd.read_csv(
-    os.path.join(DATA_DIRECTORY, "station_list.csv"), encoding="latin-1"
+    os.path.join(DATA_DIRECTORY, "station_list.csv"), encoding="utf-8"
 )
 temperature_data = pd.read_csv(
-    os.path.join(DATA_DIRECTORY, "temperature_data.csv"), encoding="latin-1"
+    os.path.join(DATA_DIRECTORY, "temperature_data.csv"), encoding="utf-8"
 )
 precipitation_data = pd.read_csv(
-    os.path.join(DATA_DIRECTORY, "precipitation_data.csv"), encoding="latin-1"
+    os.path.join(DATA_DIRECTORY, "precipitation_data.csv"), encoding="utf-8"
 )
 
 
 def get_matching_stations(station_list, station_name):
     matching_stations = []
-    for index, row in station_list.iterrows():
+    for _, row in station_list.iterrows():
         station = row["Stationsname"].strip().lower()
         if station_name in station:
             matching_stations.append(row)
@@ -67,54 +73,57 @@ def get_precipitation(matching_stations):
     return matching_rows
 
 
-matching_stations = get_matching_stations(station_list, station_name)
+def determine_output():
+    station_name = input("Enter the name of the station: ").strip().lower()
 
-matching_temperatures = get_temperature(matching_stations)
-matching_precipitaitons = get_precipitation(matching_stations)
+    matching_stations = get_matching_stations(station_list, station_name)
+    matching_temperatures = get_temperature(matching_stations)
+    matching_precipitaitons = get_precipitation(matching_stations)
 
-results = {}
+    results = {}
 
-for row in matching_stations:
-    data = {
-        "station_name": row["Stationsname"].strip(),
-        "bundesland": row["Bundesland"].strip(),
-    }
-    results[row["Stations_id"]] = data
+    # Create a dictionary to populate and output relevant data
+    for row in matching_stations:
+        data = {
+            "station_name": row["Stationsname"].strip(),
+            "bundesland": row["Bundesland"].strip(),
+        }
 
-for row in matching_temperatures:
-    for column, value in row.items():
-        if column == "Jahr":
-            results[row["Stations_id"]]["average_temperature"] = row["Jahr"]
-        else:
-            # TODO: Do manually
-            pass
+        results[row["Stations_id"]] = data
+
+    for row in matching_temperatures:
+        accumulated_temperature = 0
+        if row["Stations_id"] in results:
+            for column, value in row.items():
+                match = difflib.get_close_matches(column, MONTHS_LIST, n=1, cutoff=0.6)
+
+                if match:
+                    accumulated_temperature += float(value)
+
+        average_temperature = round(accumulated_temperature / 12, 2)
+        results[row["Stations_id"]]["average_temperature"] = average_temperature
+
+    for row in matching_precipitaitons:
+        accumulated_precipitation = 0
+        if row["Stations_id"] in results:
+            for column, value in row.items():
+                match = difflib.get_close_matches(column, MONTHS_LIST, n=1, cutoff=0.6)
+
+                if match:
+                    accumulated_precipitation += float(value)
+
+        average_precipitation = round(accumulated_precipitation / 12, 2)
+        results[row["Stations_id"]]["average_precipitation"] = average_precipitation
+
+    for index, (key, value) in enumerate(results.items(), start=1):
+        print(
+            f"{index}. Station_ID: {key}\n"
+            f"   Station: {value.get('station_name', 'N/A')}\n"
+            f"   State: {value.get('bundesland', 'N/A')}\n"
+            f"   Average Temperature: {value.get('average_temperature', 'N/A')}\n"
+            f"   Average Precipitation: {value.get('average_precipitation', 'N/A')}\n"
+        )
 
 
-for row in matching_temperatures:
-    for column, value in row.items():
-        if column == "Jahr":
-            # The temperature is already averaged in "Jahr" column
-            results[row["Stations_id"]]["average_temperature"] = value
-            print(value)
-        else:
-            # Calculate manually
-            pass
-
-for row in matching_precipitaitons:
-    for column, value in row.items():
-        if column == "Jahr":
-            # We add a new column, take the total precipitaiton and divide it by 12
-            results[row["Stations_id"]]["average_precipitation"] = round(
-                row["Jahr"] / 12, 2
-            )
-        else:
-            # Calculate manually
-            pass
-
-for index, (key, value) in enumerate(results.items(), start=1):
-    print(
-        f"{index}. Station: {value.get('station_name', 'N/A')}, "
-        f"State: {value.get('bundesland', 'N/A')}, "
-        f"Average Temperature: {value.get('average_temperature', 'N/A')}, "
-        f"Average Precipitation: {value.get('average_precipitation', 'N/A')}"
-    )
+while True:
+    determine_output()
